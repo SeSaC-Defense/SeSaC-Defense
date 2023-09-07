a9434497f9118f398cc17917d87cb97311c9c7ae
Garden <supe128@naver.com>
2023-09-07 16:50:37 +0900

    fix(script): tile.script 추가 - tile.script 추가

---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool IsBuildTower
+    {
+        set; get;
+    }
+
+    private void Awake()
+    {
+        IsBuildTower = false;
+    }
+}
