9ef5f4453efc23f98231ce8f2ff925807bd1cd2c
Gyeongyong Park <pky941129@gmail.com>
2023-09-14 14:16:24 +0900

    fix(scene, script): UI 일부 수정 및 테스트용 씬 추가
    일부 스크립트 이름 변경
    - UI 관련 스크립트 이름에서 UI 제거
    
    테스트용 게임 씬 추가

--- Assets/Script/UIs/UIActiveToggle.cs
+++ Assets/Script/UIs/UIActiveToggle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIPanelToggle : MonoBehaviour
+public class UIActiveToggle : MonoBehaviour
 {
     public void CloseUI()
     {
